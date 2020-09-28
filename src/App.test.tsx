52101a29cdfb1d0e4d3b76c1d8dcd81f7a270b89
barosanuemailtest <barosanu.email.test@gmail.com>
2020-09-28 17:55:19 +0300

    Enzime setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+//import { render } from '@testing-library/react';
+import Enzime, { shallow } from 'enzyme';
+import EnzimeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
+Enzime.configure({
+  adapter: new EnzimeAdapter()
+})
+
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const wrapper = shallow(<App />)
+  expect(wrapper).toBeTruthy();
+  // const { getByText } = render(<App />);
+  // const linkElement = getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
 });
